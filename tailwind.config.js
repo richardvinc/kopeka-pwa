fb6206f29892adf4f2fb8d7e6107729811f95285
richardvinc <vincrichard@live.com>
2024-05-05 08:52:16 +0700

    feat(): change font to 'jakarta'

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jakarta: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
