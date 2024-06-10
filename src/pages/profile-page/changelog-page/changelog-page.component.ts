2decd71a5b3a5ada1ec255dd4e00814d824b8359
richardvinc <vincrichard@live.com>
2024-06-10 22:47:32 +0700

    fix(): fix changelog page styling

--- src/pages/profile-page/changelog-page/changelog-page.component.ts
+++ src/pages/profile-page/changelog-page/changelog-page.component.ts
@@ -6,23 +6,11 @@ import { AppConfigService } from '@app/shared/services/config/app/app-config.ser
 @Component({
   selector: 'app-changelog-page',
   standalone: true,
-  template: `
-    <div
-      class="max-w-sm mx-auto bg-white rounded-lg overflow-hidden shadow-lg mb-4 p-4"
-    >
-      <p class="mb-4">Versi Aplikasi: {{ appVersion }}</p>
-      <h1 class="font-bold">Sejarah Pengembangan</h1>
-      <p class="text-left">{{ changelogMessage }}</p>
-    </div>
-  `,
+  templateUrl: './changelog-page.component.html',
 })
 export class ChangelogPageComponent {
   appVersion = app.version;
-  changelogMessage =
-    app.changelog
-      .find((change) => change.version === app.version)
-      ?.changes.map((c) => `- ${c}`)
-      .join('\n') || 'No changelog found';
+  changelogMessage = app.changelog;
 
   constructor(private appConfigService: AppConfigService) {
     this.appConfigService.setPageTitle('Sejarah Pengembangan');
